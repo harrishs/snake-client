d6d9a01e5953d745e485d33feeea1b9139ea5464
Harrish S <harrish.s@hotmail.ca>
2019-08-28 16:57:11 +0000

    add name

--- client.js
+++ client.js
@@ -8,6 +8,12 @@ const connect = function() {
 
   conn.setEncoding("utf8");
 
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+  });
+
+  conn.write("Name: HS1");
+
   conn.on("data", data => {
     console.log(data);
   });
