ebe2929a9deb9c8a6d98908e1b84b0c803d055d1
Harrish S <harrish.s@hotmail.ca>
2019-08-28 17:30:03 +0000

    export input functions from seperate file

--- play.js
+++ play.js
@@ -1,19 +1,5 @@
 const { connect } = require("./client");
+const { setupInput } = require("./input");
 console.log("Connecting ...");
 connect();
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  function handleUserInput() {
-    stdin.on("data", key => {
-      if (key === "\u0003") {
-        process.exit();
-      }
-    });
-  }
-  stdin.resume();
-  return stdin;
-};
 setupInput();
